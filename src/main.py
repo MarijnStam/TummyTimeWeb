5f3db2b6a9df42f19fc9bcd40847ddded817e0bb
Marijn <marijnstam95@gmail.com>
2023-07-19 16:48:33 +0200

    add an endpoint for meals

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Request
 from pydantic import BaseModel
 
+from typing import Set
 from typing_extensions import Annotated
 
 import src.dbHelper as db
 
 class Feel(BaseModel):
     feel: Annotated[int, Body(gt=0, le=10)]
+    timestamp: str
+
+class Food(BaseModel):
+    ingredients: Set[str] = set()
+    mealName: str
     timestamp: str
 
 app = FastAPI()
@@ -20,3 +26,9 @@ async def root():
 async def create_feel_entry(feeling: Feel):
     db.setFeel(feeling.feel, feeling.timestamp)
     return feeling
+
+@app.put("/food/")
+async def create_food_entry(food: Food):
+    print(food.mealName)
+    print(food.ingredients)
+    return food
