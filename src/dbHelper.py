592aff9317c7380812c5761b76ab2b141b68aa01
Marijn Stam <marijnstam@betronic.nl>
2023-07-21 14:00:38 +0200

    update dbhelper to be more complete, edit api a little

--- src/dbHelper.py
+++ src/dbHelper.py
@@ -3,22 +3,28 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
 class SQLite:
     """
     This class is a context manager for the database.
     Use this for handling db operations within this file
     """
+
     def __init__(self):
         self.dbName = os.path.join(BASE_DIR, "TummyTime.db")
+
     def __enter__(self):
         self.conn = sqlite3.connect(self.dbName)
         return self.conn.cursor()
+
     def __exit__(self, type, value, traceback):
         self.conn.commit()
         self.conn.close()
 
+
 class NotFoundError(Exception):
     pass
+
 
 def setFeel(feel: int, timestamp: str):
     """
@@ -31,14 +37,23 @@ def setFeel(feel: int, timestamp: str):
 
     timestamp : `str`\n
         timestamp of the recorded feel
+    
+    Returns
+    ----------
+    rowid : `int`\n
+        id of the inserted feel
 
     """
-    
+
     with SQLite() as cur:
         try:
-            cur.execute(f"INSERT INTO Feelings (Feel, Timestamp) VALUES ('{feel}', '{timestamp}')")
+            cur.execute(
+                f"INSERT INTO Feelings (Feel, Timestamp) VALUES ('{feel}', '{timestamp}')"
+            )
+            return cur.lastrowid
         except sqlite3.Error as e:
             raise sqlite3.Error(e)
+
 
 def setMeal(name: str):
     """
@@ -46,16 +61,81 @@ def setMeal(name: str):
 
     Parameters
     ----------
-    name : `int`\n
+    name : `str`\n
         Name of the meal
+    
+    Returns
+    ----------
+    rowid : `int`\n
+        id of the inserted meal
 
     """
-    
+
     with SQLite() as cur:
         try:
-            cur.execute(f"INSERT INTO Meals (Name) VALUES ('{name}'')")
+            cur.execute(f"INSERT INTO Meals (Name) VALUES ('{name}')")
+            return cur.lastrowid
         except sqlite3.Error as e:
             raise sqlite3.Error(e)
+
+
+def getMeal(name: str = "", id: int = 0):
+    """
+    Gets a meal from the db either by name or by unique id
+
+    Parameters
+    ----------
+    name : `str`\n
+        Name of the meal
+    id   : `int`\n
+        Unique ID of the meal
+
+    Returns
+    ----------
+    meal : `tuple`\n
+        Found meal
+    """
+    rows = None
+
+    # First validate the input and determine how we retrieve the meal
+    if name == "" and id == 0:
+        return
+    if id != 0:
+        query = f"SELECT * FROM Meals WHERE MealID='{id}'"
+        sqlParameter = id
+    else:
+        query = f"SELECT * FROM Meals WHERE Name='{name}'"
+        sqlParameter = name
+
+    with SQLite() as cur:
+        try:
+            cur.execute(query)
+            rows = cur.fetchone()
+            if rows is None:
+                raise NotFoundError(f"Unable to find Meal: {sqlParameter}")
+
+            return rows[0]
+
+        except sqlite3.Error as e:
+            raise sqlite3.Error(e)
+
+
+def getAllMeals():
+    """
+    Gets all meals from the DB
+
+    Returns
+    ----------
+        List of tuples with Meals : `List`
+    """
+
+    with SQLite() as cur:
+        try:
+            cur.execute("SELECT * FROM Meals;")
+            return cur.fetchall()
+        except sqlite3.Error as e:
+            raise sqlite3.Error(e)
+
 
 def setIngredient(name: str):
     """
@@ -65,14 +145,79 @@ def setIngredient(name: str):
     ----------
     name : `str`\n
         Name of the ingredient
+
+    Returns
+    ----------
+    rowid : `int`\n
+        id of the inserted ingredient
     """
-    
+
     with SQLite() as cur:
         try:
             cur.execute(f"INSERT INTO Ingredients (Name) VALUES ('{name}')")
+            return cur.lastrowid
         except sqlite3.Error as e:
             raise sqlite3.Error(e)
-        
+
+
+def getIngredient(name: str = "", id: int = 0):
+    """
+    Gets a ingredient from the db either by name or by unique id
+
+    Parameters
+    ----------
+    name : `str`\n
+        Name of the ingredient
+    id   : `int`\n
+        Unique ID of the ingredient
+
+    Returns
+    ----------
+    ingredient : `tuple`\n
+        Found ingredient
+    """
+    rows = None
+
+    # First validate the input and determine how we retrieve the ingredient
+    if name == "" and id == 0:
+        return
+    if id != 0:
+        query = f"SELECT * FROM Ingredients WHERE IngredientID='{id}'"
+        sqlParameter = id
+    else:
+        query = f"SELECT * FROM Ingredients WHERE Name='{name}'"
+        sqlParameter = name
+
+    with SQLite() as cur:
+        try:
+            cur.execute(query)
+            rows = cur.fetchone()
+            if rows is None:
+                raise NotFoundError(f"Unable to find Ingredients: {sqlParameter}")
+
+            return rows[0]
+
+        except sqlite3.Error as e:
+            raise sqlite3.Error(e)
+
+
+def getAllIngredients():
+    """
+    Gets all Ingredients from the DB
+
+    Returns
+    ----------
+        List of tuples with Ingredients : `List`
+    """
+
+    with SQLite() as cur:
+        try:
+            cur.execute("SELECT * FROM Ingredients;")
+            return cur.fetchall()
+        except sqlite3.Error as e:
+            raise sqlite3.Error(e)
+
+
 def setMealEntry(mealID: int, timestamp: str):
     """
     Sets a Meal Entry into the db
@@ -83,13 +228,22 @@ def setMealEntry(mealID: int, timestamp: str):
         ID of the meal
     timestamp : `str`\n
         time the meal was eaten
+
+    Returns
+    ----------
+    rowid : `int`\n
+        id of the inserted MealEntry
     """
-    
+
     with SQLite() as cur:
         try:
-            cur.execute(f"INSERT INTO MealEntry (MealID, Timestamp) VALUES ('{mealID}', '{timestamp}')")
+            cur.execute(
+                f"INSERT INTO MealEntry (MealID, Timestamp) VALUES ('{mealID}', '{timestamp}')"
+            )
+            return cur.lastrowid
         except sqlite3.Error as e:
             raise sqlite3.Error(e)
+
 
 # --------------------------- JOIN TABLE FUNCTIONS ------------------------------- #
 # -------------------------------------------------------------------------------- #
@@ -104,12 +258,15 @@ def setMealEntry_Ingredients(mealEntryID: int, ingredientID: int):
     ingredientID : `int`\n
         ID of the ingredient
     """
-    
+
     with SQLite() as cur:
         try:
-            cur.execute(f"INSERT INTO MealEntry_Ingredients (EntryID, IngredientID) VALUES ('{mealEntryID}', '{ingredientID}')")
+            cur.execute(
+                f"INSERT INTO MealEntry_Ingredients (EntryID, IngredientID) VALUES ('{mealEntryID}', '{ingredientID}')"
+            )
         except sqlite3.Error as e:
             raise sqlite3.Error(e)
+
 
 def setBase_Ingredients(mealID: int, ingredientID: int):
     """
@@ -118,14 +275,15 @@ def setBase_Ingredients(mealID: int, ingredientID: int):
     Parameters
     ----------
     mealID : `int`\n
-        ID of the meal 
+        ID of the meal
     ingredientID : `int`\n
         ID of the ingredient
     """
-    
+
     with SQLite() as cur:
         try:
-            cur.execute(f"INSERT INTO Base_Ingredients (MealID, IngredientID) VALUES ('{mealID}', '{ingredientID}')")
+            cur.execute(
+                f"INSERT INTO Base_Ingredients (MealID, IngredientID) VALUES ('{mealID}', '{ingredientID}')"
+            )
         except sqlite3.Error as e:
             raise sqlite3.Error(e)
-
--- src/main.py
+++ src/main.py
@@ -6,29 +6,49 @@ from typing_extensions import Annotated
 
 import src.dbHelper as db
 
+
 class Feel(BaseModel):
     feel: Annotated[int, Body(gt=0, le=10)]
     timestamp: str
 
+
 class Food(BaseModel):
     ingredients: Set[str] = set()
     mealName: str
-    timestamp: str
 
 app = FastAPI()
 
+@app.get("/ingredients/")
+#TODO Format to return only ingredient names and not IDs
+async def get_ingredients() -> Set[str]:
+    return db.getAllIngredients() 
 
-@app.get("/hello_world")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/meal_names/")
+#TODO Format to return only ingredient names and not IDs
+async def get_meal_names() -> Set[str]:
+    return db.getAllMeals()
 
 @app.put("/feel/")
-async def create_feel_entry(feeling: Feel):
+async def feel_entry(feeling: Feel):
     db.setFeel(feeling.feel, feeling.timestamp)
     return feeling
 
-@app.put("/food/")
-async def create_food_entry(food: Food):
-    print(food.mealName)
-    print(food.ingredients)
+@app.put("/new_meal/")
+async def new_meal(food: Food):
+    #TODO Handle case where meal is already in db
+    mealID = db.setMeal(food.mealName)
+
+    # Store all the ingredients and update the join table with ingredients for this meal
+    # TODO Handle case where ingredient is already in db
+    for ingredient in food.ingredients:
+        ingredientID = db.setIngredient(ingredient)
+        db.setBase_Ingredients(mealID, ingredientID)
+
     return food
+
+@app.put("/meal_entry/")
+async def meal_entry(food: Food):
+    return food
+
+
+
